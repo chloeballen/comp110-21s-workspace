e10bb61e458ad7a7c9d26df607e6e30095aaa236
Chloe Allen <chloeall@live.unc.edu>
2023-10-04 22:04:52 -0400

    Exercise 1 Part 1

--- exercises/ex01/fortune_cookie.py
+++ exercises/ex01/fortune_cookie.py
@@ -13,3 +13,32 @@ from random import randint
 
 
 # Begin your solution here...
+
+
+import random
+
+print("Welcome to the Underwhelming Fortune Teller")
+
+start = input('Press enter to begin')
+
+
+print("Your fortune cookie says...")
+
+fortune = random.randint(1,4)
+
+if fortune == 1:
+    print("You will find cash you forgot you had.")
+else:
+    if fortune == 2:
+        print("You will eat your favorite meal but it's a little cold.")
+    else:
+        if fortune == 3:
+            print("You will get to work from home tomorrow afternoon.")
+        else:
+            if fortune == 4:
+                print("You will see a cool bird but it poops on you car.")
+        
+print("Now, go spread positive vibes!")
+
+
+
